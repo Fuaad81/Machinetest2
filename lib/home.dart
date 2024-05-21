9a45e57989f25aadb97bbba76d98a89c07bf6665
Fuaad81 <hpfuaad@gmail.com>
2024-05-21 12:20:37 +0530

    machinetest2

---
+++ lib/home.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_brace_in_string_interps
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App",style: TextStyle(
+          color: Colors.blue,
+          fontSize: 18
+        ),),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text("${counter}"),
+              )
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: ElevatedButton(onPressed: (){
+              setState(() {
+                counter++;
+              });
+            }, child: Icon(Icons.add)),
+          )
+        ],
+      ),
+    );
+  }
+}
